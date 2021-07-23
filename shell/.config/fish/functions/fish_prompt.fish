50c59cce704c78e06fc82790c62fa863fda153ca
Ethan Budd <ethan.budd@ibm.com>
2021-07-23 14:32:55 -0500

    rework of the prompt

--- shell/.config/fish/functions/fish_prompt.fish
+++ shell/.config/fish/functions/fish_prompt.fish
@@ -1,18 +1,33 @@
+set __fish_git_prompt_show_informative_status
+set __fish_git_prompt_showcolorhints
+set __fish_git_prompt_showupstream "informative"
+set -g fish_prompt_pwd_dir_length 0
+
 function fish_prompt
-	set_color brblack
+        set -l last_status $status
+
+	set_color magenta
 	echo -n "["(date "+%H:%M")"] "
 	set_color blue
-	echo -n (whoami)"@"(hostname -s)
+	printf '%s' $USER
+        echo -n '@'
+        echo -n (hostname -s)
 	if [ $PWD != $HOME ]
 		set_color brblack
 		echo -n ':'
 		set_color yellow
-		echo -n (basename $PWD)
+		echo -n (prompt_pwd)
 	end
 	set_color green
 	printf '%s ' (__fish_git_prompt)
+        if not test $last_status -eq 0
+            set_color $fish_color_error
+        else
+            set_color brblack
+        end
+	printf '\n[%s] ' $last_status
 	set_color red
-	echo -n '> '
+        echo -n '-> '
 	set_color normal
 end
 

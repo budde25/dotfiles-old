5a55a494dc43cd64f0c40e1de6e2e3bccbe777b7
Ethan Budd <ethan.budd@ibm.com>
2021-08-09 10:43:42 -0500

    passwordless ssh

--- shell/.config/fish/functions/ssh.fish
+++ shell/.config/fish/functions/ssh.fish
@@ -1,10 +1,12 @@
 function ssh
-	switch $argv[1]
-	case "*.local"
-		env TERM=xterm /usr/bin/ssh $argv
-	case "*.cs.wisc.edu"
-		env TERM=xterm /usr/bin/ssh $argv
-	case "*"
-		/usr/bin/ssh $argv
-	end
+    switch $argv[1]
+    case "*.local"
+        env TERM=xterm /usr/bin/ssh $argv
+    case "*.cs.wisc.edu"
+    	env TERM=xterm /usr/bin/ssh $argv
+    case "*.ibm.com" or "tnet*" or "dumbo*"
+        ~/dotfiles/scripts/kerberos_ssh ssh $argv
+    case "*"
+    	/usr/bin/ssh $argv
+    end
 end
